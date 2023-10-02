4218c317ffb36bb75442a13bce9ffe1f2e7eb23a
mohamed0690 <mohamedmahrous069@gmail.com>
2023-10-02 21:04:29 +0300

    first milestone

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import BenefitsOverview from "./components/BenefitsOverview";
 import Navbar from "./components/shared/Navbar";
+import OurPartners from "./components/OurPartners";
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <BenefitsOverview></BenefitsOverview>
+      <OurPartners></OurPartners>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "@fortawesome/fontawesome-free/js/all.min.js";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
 import "./i18n";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+root.render(<App />);
