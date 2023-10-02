44ea53b5d7f3ea24963b5ea71ee181b19f0a2335
HagerMustafa <haagaarmustafa@gmail.com>
2023-10-02 21:22:03 +0300

    first milestone

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import About from "./components/About";
+import ContactUSForm from "./components/ContactUSForm";
 import Navbar from "./components/shared/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <About ></About>
+      <ContactUSForm className="mt-5"></ContactUSForm>
     </div>
   );
 }
