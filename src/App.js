593ef688791c159b313f324a915baaa6c8f4d446
Mahmoud-rifaat <mahmoud.ref2at.dev@gmail.com>
2023-10-02 22:56:38 +0300

    u

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Slider from "./components/Slider";
+import Footer from "./components/shared/Footer";
 import Navbar from "./components/shared/Navbar";
 
 function App() {
   return (
-    <div className="App h-100 d-flex-column">
+    <div className="App">
       <Navbar></Navbar>
-      <Slider />
+      <Slider className="mx-auto" />
+      <Footer></Footer>
     </div>
   );
 }
