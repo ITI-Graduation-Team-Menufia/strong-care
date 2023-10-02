c50ef150cff8e4bcff3ed2dffc989f15a79f537c
Mahmoud-rifaat <mahmoud.ref2at.dev@gmail.com>
2023-10-02 13:27:43 +0300

    Setting font style, adding logo, cleaned some files

--- src/i18n.js
+++ src/i18n.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useTranslation, initReactI18next } from "react-i18next";
+import { initReactI18next } from "react-i18next";
 import i18n from "i18next";
 import enTranslation from "./languages/resources/en.json";
 import arTranslation from "./languages/resources/ar.json";
@@ -17,7 +17,7 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: "en", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+    lng: "ar", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
 
