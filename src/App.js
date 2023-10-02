5d3ba7ba74af08c58c6258087caca7ebedbf1cd3
tarek-abdo <tabdelnaby06@gmail.com>
2023-10-02 22:35:44 +0300

    resgistration section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/shared/Navbar";
+import Register from "./components/shared/Register";
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Register/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./i18n";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "@fortawesome/fontawesome-free/js/all.min.js";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
