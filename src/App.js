e24e820348e79ffd264a7a90857a9fd14f58a69f
Mahmoud-rifaat <mahmoud.ref2at.dev@gmail.com>
2023-10-02 22:26:56 +0300

    completeing navbar and footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Footer from "./components/shared/Footer";
 import Navbar from "./components/shared/Navbar";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App h-100 d-flex-column">
       <Navbar></Navbar>
+      <Footer></Footer>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./i18n";
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
