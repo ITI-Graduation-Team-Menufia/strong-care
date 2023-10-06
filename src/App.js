b9312c2cb565f9911b4bc94f3ba4a99a91cdc7ae
tarek-abdo <tabdelnaby06@gmail.com>
2023-10-06 11:33:53 +0300

    review

--- src/App.js
+++ src/App.js
@@ -1,36 +1,29 @@
 import "./App.css";
-<<<<<<< HEAD
-// import Review from "./components/shared/Review";
-import ConfirmationCode from "./components/shared/ConfirmationCode";
+// import BenefitsOverview from "./components/BenefitsOverview";
+// import Slider from "./components/Slider";
 // import Navbar from "./components/shared/Navbar";
 // import Register from "./components/shared/Register";
+// import OurPartners from "./components/OurPartners";
+import Review from "./components/shared/Review";
 
 function App() {
   return (
     <div >
       {/* <Navbar></Navbar>
-      <Register/> */}
-      <ConfirmationCode/>
-      {/* <Review/> */}
-
-=======
-import BenefitsOverview from "./components/BenefitsOverview";
-import Slider from "./components/Slider";
-import Navbar from "./components/shared/Navbar";
-import Register from "./components/shared/Register";
-import OurPartners from "./components/OurPartners";
-
-function App() {
-  return (
-    <div className="App h-100 d-flex-column">
-      <Navbar></Navbar>
       <Register/>
       <BenefitsOverview></BenefitsOverview>
       <OurPartners></OurPartners>
-      <Slider />
->>>>>>> b7d47dd87c67b394e3b5171fdf7b1b5ff803ed5e
+      <Slider /> */}
+      <Review/>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
