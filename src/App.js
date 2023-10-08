270fa2001b07624e4ad59deac8c4d2ab5f208e27
tarek-abdo <tabdelnaby06@gmail.com>
2023-10-08 19:45:37 +0300

    compensation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import "./App.css";
-import FormOne from "./components/FormOne";
-import FormTwo from "./components/FormTwo";
-import { Route, Routes } from "react-router-dom";
-import { Footer, Navbar } from "./components/shared";
-import { CompanyProfile } from './components/company';
-import Review from './components/Review'
-import ConfirmationCode from './components/ConfirmationCode'
-import Home from "./pages/Home/Home";
+// import FormOne from "./components/FormOne";
+// import FormTwo from "./components/FormTwo";
+// import { Route, Routes } from "react-router-dom";
+// import { Footer, Navbar } from "./components/shared";
+// import { CompanyProfile } from './components/company';
+// import Review from './components/Review'
+// import ConfirmationCode from './components/ConfirmationCode'
+// import Home from "./pages/Home/Home";
+import Compensation from "./components/Compensation";
 
 
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
+      {/* <Navbar></Navbar>
       <Home></Home>
       <Footer></Footer>
       <FormOne></FormOne>
@@ -22,7 +23,8 @@ function App() {
       <ConfirmationCode></ConfirmationCode>
       <Routes>
         <Route path="companyProfile" element={<CompanyProfile></CompanyProfile>}></Route>
-      </Routes>
+      </Routes> */}
+      <Compensation />
     </div>
   );
 }
--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,12 +1,7 @@
 import React from 'react'
-<<<<<<< HEAD
-import check from "../../assets/images/check.png";
-import { Trans } from "react-i18next";
-=======
 import check from "../assets/images/check.png";
 import { Trans } from "react-i18next";
 
->>>>>>> 2bf5646d9abb4e897afa1c4a57b040dce612fb5a
 export default function Review() {
   return (
     <div dir="rtl" class="container d-flex justify-content-center align-items-center flex-column " style={{display: "flex", height: "100vh"}}>
@@ -16,11 +11,7 @@ export default function Review() {
           <img src={check} alt="..." class="img-fluid"/>
         </div>
         <div class="col-md-4">
-<<<<<<< HEAD
-          <p class="text-center"><Trans i18nKey="waiting-review2" values="ar">طلبك قيد المراجعة وسيتم الرد عليك في اقرب وقت</Trans></p>
-=======
         <p class="text-center"><Trans i18nKey="waiting-review2" values="ar"></Trans></p>
->>>>>>> 2bf5646d9abb4e897afa1c4a57b040dce612fb5a
         </div>
       </div>
 </div>
