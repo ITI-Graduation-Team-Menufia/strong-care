adbcdf695f471e4da8367f79b0040a7b96ff5256
Mahmoud-rifaat <mahmoud.ref2at.dev@gmail.com>
2023-10-16 22:34:39 +0300

    Adding the About-us component

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import { Trans } from "react-i18next";
 import logo from "../../assets/images/logo.png";
 import "./Navbar.css";
+import { Link } from "react-router-dom";
+import { Home } from "../../pages/home/Home";
+import { FormOne } from "../FormOne";
 
 export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary" dir="rtl">
       <div className="container fw-bold">
         <span className="navbar-brand" >
-          <img src={logo} alt="Bootstrap" width="100" />
+          <Link to="/">
+            <img src={logo} alt="Bootstrap" width="100" />
+          </Link>
         </span>
         <button
           className="navbar-toggler"
@@ -27,14 +32,18 @@ export default function Navbar() {
         >
           <ul className="navbar-nav ">
             <li className="nav-item">
-              <span className="nav-link" aria-current="page" >
-                <Trans i18nKey="home"></Trans>
-              </span>
+              <Link to="/">
+                <span className="nav-link" aria-current="page" >
+                  <Trans i18nKey="home"></Trans>
+                </span>
+              </Link>
             </li>
             <li className="nav-item">
-              <span className="nav-link" aria-current="page" >
-                <Trans i18nKey="claimsAndCompensations"></Trans>
-              </span>
+              <Link to='compensation'>
+                <span className="nav-link" aria-current="page" >
+                  <Trans i18nKey="claimsAndCompensations"></Trans>
+                </span>
+              </Link>
             </li>
             <li className="nav-item">
               <span className="nav-link" >
@@ -42,35 +51,47 @@ export default function Navbar() {
               </span>
             </li>
             <li className="nav-item">
-              <span className="nav-link" >
-                <Trans i18nKey="aboutUs"> </Trans>
-              </span>
+              <Link to='aboutus'>
+                <span className="nav-link" >
+                  <Trans i18nKey="aboutUs"> </Trans>
+                </span>
+              </Link>
             </li>
             <li className="nav-item dropdown">
               <span
                 className="nav-link dropdown-toggle p-1"
-                
                 role="button"
                 data-bs-toggle="dropdown"
                 aria-expanded="false"
               >
                 <i className="bi bi-person p-1 lead"></i>
-                <Trans i18nKey="login"></Trans>
+                <Trans i18nKey="register"></Trans>
               </span>
-              <ul className="dropdown-menu">
-                <li>
-                  <span className="dropdown-item text-center" >
-                    <i className="bi bi-people-fill p-1 lead"></i>
-                    <Trans i18nKey="loginAsCorporation"></Trans>
-                  </span>
+              <ul className="dropdown-menu text-center">
+                <li className="w-100">
+                  <Link to="signup">
+                    <span className="dropdown-item text-center" >
+                      <i className="bi bi-people-fill p-1 lead"></i>
+                      <Trans i18nKey="signUpAsCorporation"></Trans>
+                    </span>
+                  </Link>
                 </li>
-                <li>
-                  <span className="dropdown-item text-center" >
-                    <i className="bi bi-person-fill p-1 lead"></i>
-                    <Trans i18nKey="loginAsIndividual"></Trans>
-                  </span>
+                <li className="w-100">
+                  <Link to="register">
+                    <span className="dropdown-item text-center" >
+                      <i className="bi bi-person-fill p-1 lead"></i>
+                      <Trans i18nKey="signUpAsIndividual"></Trans>
+                    </span>
+                  </Link>
                 </li>
               </ul>
+            </li>
+            <li className="nav-item">
+              <Link to='signin'>
+                <span className="nav-link" >
+                  <Trans i18nKey="login"> </Trans>
+                </span>
+              </Link>
             </li>
             {/* Temporary */}
             {/* <li className="nav-item">
