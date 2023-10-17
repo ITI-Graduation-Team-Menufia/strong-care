794e50f472422ddbf7deeb932ebcc7e47573fc6c
Mahmoud-rifaat <mahmoud.ref2at.dev@gmail.com>
2023-10-17 22:05:35 +0300

    Creating generic axois crud functions

--- src/APIs/crud.js
+++ src/APIs/crud.js
@@ -3,8 +3,15 @@ import { baseURL } from "./baseURL";
 
 
 
-let getAllRecords = (path) => axios.get(`${baseURL}${path}`);
+let getAllRecords = (resourceName) => axios.get(`${baseURL}${resourceName}`);
+let getRecordById = (resourceName, id) => axios.get(`${baseURL}${resourceName}/${id}`);
+let addRecord = (resourceName, recordData) => axios.post(`${baseURL}${resourceName}`, recordData);
+let editRecord = (resourceName, recordId, updatedRecord) =>
+    axios.put(`${baseURL}${resourceName}/${recordId}`, updatedRecord, {
+        headers: {
+            'Content-Type': 'multipart/form-data', // Important for file uploads
+        }
+    })
+let deleteRecord = (resourceName, recordId) => axios.delete(`${baseURL}${resourceName}/${recordId}`);
 
-
-
-export { getAllRecords }
+export { getAllRecords, getRecordById, addRecord, editRecord, deleteRecord }
